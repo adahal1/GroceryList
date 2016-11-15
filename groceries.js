e15b2f53add7843174c073c59ed47d98955d2351
adahal1 <adahal1@mail.csuchico.edu>
2016-11-14 16:26:58 -0800

    button added

--- groceries.js
+++ groceries.js
@@ -26,8 +26,19 @@ function addItem(){
     var input = document.getElementById("newItem").value;
     var list = document.getElementById("listDisplay");
     var item = document.createElement("li");
+    var btnClose;
+    btnClose = document.createElement("button");
+    btnClose.classList.add("btn");
+    btnClose.classList.add("btn-danger");
+    btnClose.classList.add("btn-xs");
+    var iconClose;
+    iconClose = document.createElement("span");
+    iconClose.classList.add("glyphicon");
+    iconClose.classList.add("glyphicon-remove");
+    btnClose.appendChild(iconClose);
     var itemName = document.createTextNode(input);
     item.appendChild(itemName);
+    item.appendChild(btnClose);
     list.appendChild(item);
     document.getElementById("newItem").value = "";
 }
